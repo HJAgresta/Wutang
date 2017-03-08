d2bdb7e117308fd54c951207f5287c35f48ad824
Jake Recoon <jrecoon@gmail.com>
2017-03-07 20:26:43 -0500

    Can pick up flashight, but does not aim properly yet

---
+++ WuTang/Assets/Scripts/getFlashlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getFlashlight : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("e"))
+        {
+            Debug.Log(Vector3.Distance(Player.transform.position, this.transform.position) < 15);
+            if (Vector3.Distance(Player.transform.position, this.transform.position) < 15)
+            {
+                this.transform.SetParent(Player.transform);
+            }
+        }
+	}
+}
