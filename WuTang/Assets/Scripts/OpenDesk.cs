23ece4d4816b9cc1097e043937c47c80d0975c4e
Jake Recoon <jrecoon@gmail.com>
2017-03-09 18:54:01 -0500

    First Three rooms are upgraded

---
+++ WuTang/Assets/Scripts/OpenDesk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDesk : PuzzleObject
+{
+    bool go = false;
+    float oldPos;
+    bool next = false;
+    public PuzzleObject act;
+
+    public override void activate()
+    {
+        go = true;
+    }
+
+    void Start()
+    {
+        oldPos = gameObject.transform.position.z;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (go && 5 > Mathf.Abs(oldPos - gameObject.transform.position.z))
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, -Time.deltaTime * 10));
+        }
+        else if (next == false)
+        {
+            next = true;
+        }
+
+        if (next)
+        {
+            act.activate();
+        }
+    }
+}
--- WuTang/Assets/Scripts/UnlockedDoor.cs
+++ WuTang/Assets/Scripts/UnlockedDoor.cs
@@ -6,21 +6,26 @@ public class UnlockedDoor : MonoBehaviour {
 
     public GameObject playerpub;
     private Vector3 playerpos;
+    private bool opening = false;
+    private Vector3 vec;
+    private Vector3 endVec = new Vector3(0f, 270f, 0f);
+
     // Update is called once per frame
     void Update()
     {
         playerpos = playerpub.transform.position;
 
-        if (Vector3.Distance(playerpos, this.transform.position) < 20)
+        Debug.Log(this.transform.eulerAngles);
+
+        if (Vector3.Distance(playerpos, this.transform.position) < 20 && Input.GetKeyDown("e"))
         {
-            if (gameObject.transform.eulerAngles.y > 90f)
-            {
-                gameObject.transform.Rotate(0, -50 * Time.deltaTime, 0);
-            }
-            else
-            {
-                gameObject.transform.eulerAngles = new Vector3(0f, 90f, 0f);
-            }
+            opening = true;
+        }
+
+        if (opening)
+        {
+            vec = Vector3.Lerp(vec, endVec, Time.deltaTime * 50);
+            this.transform.eulerAngles = vec;
         }
     }
 }
