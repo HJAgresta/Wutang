fcf13deeca2012b768ed284e1a9b7cce26be28e3
Jake Recoon <jrecoon@gmail.com>
2017-03-14 22:53:34 -0400

    Random door room added, not tested yet

---
+++ WuTang/Assets/Scripts/RandomDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDoor : PuzzleObject {
+
+    public PuzzleObject act1;
+    public PuzzleObject act2;
+    public PuzzleObject act3;
+    private int num = 1000;
+    private bool unlock = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if(num == 0)
+        {
+            act1.activate();
+        }
+        else if (num == 1)
+        {
+            act2.activate();
+        }
+        else if (num == 2)
+        {
+            act3.activate();
+        }
+    }
+
+    public override void activate()
+    {
+        if (!unlock)
+        {
+            num = Random.Range(0, 2);
+        }
+    }
+}
