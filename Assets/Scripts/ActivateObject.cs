ad092702118e81d0a64f6a951f7571cceb0dd0d4
Jake Recoon <jrecoon@gmail.com>
2017-04-09 19:28:47 -0400

    Living Room functional

---
+++ Assets/Scripts/ActivateObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObject : PuzzleObject {
+
+    private bool active;
+    public PuzzleObject act;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (active && Input.GetKeyDown("e") && Vector3.Distance(player.transform.position, this.transform.position) < 15)
+        {
+            act.activate();
+        }
+	}
+
+    public override void activate()
+    {
+        active = true;
+    }
+}
