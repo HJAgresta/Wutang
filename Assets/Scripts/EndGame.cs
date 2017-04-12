ea6d7c29e92e73f36e5af7dd3976cc388ef564e1
Jake Recoon <jrecoon@gmail.com>
2017-04-12 00:17:11 -0400

    End room works, credits room just needs plaques

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndGame : PuzzleObject {
+
+    public Image img;
+    public float fadeSpeed;
+    private bool gameOver;
+
+	// Use this for initialization
+	void Start () {
+        gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameOver)
+        {
+            img.enabled = true;
+            img.color = Color.Lerp(img.color, Color.black, fadeSpeed * Time.deltaTime);
+            if(img.color.a > .95)
+            {
+                gameOver = false;
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+        else
+        {
+            img.color = Color.Lerp(img.color, Color.clear, fadeSpeed * Time.deltaTime);
+        }
+    }
+
+    public override void activate()
+    {
+        gameOver = true;
+    }
+}
