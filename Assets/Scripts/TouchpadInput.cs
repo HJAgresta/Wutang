000a1ab1addd0a7f71e62378a2c7c9c68d5e166e
Jake Recoon <jrecoon@gmail.com>
2017-04-14 19:39:35 -0400

    Wall Room copied, touchpad script redone

---
+++ Assets/Scripts/TouchpadInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchpadInput : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+    private SteamVR_TrackedController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.PadClicked += Controller_PadClicked;
+    }
+    private void Controller_PadClicked(object sender, ClickedEventArgs e)
+    {
+        if (device.GetAxis().x != 0 || device.GetAxis().y != 0)
+        {
+            Debug.Log(device.GetAxis().x + " " + device.GetAxis().y);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+    }
+}
