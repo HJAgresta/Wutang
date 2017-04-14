fac671737dafcb645b189f1b0bf131559cddf383
Jake Recoon <jrecoon@gmail.com>
2017-04-14 00:51:52 -0400

    Hallway 2 done and functional

--- Assets/Scripts/ItemHolder.cs
+++ Assets/Scripts/ItemHolder.cs
@@ -7,8 +7,10 @@ public class ItemHolder : PuzzleObject {
     public GameObject baseObject;
     public GameObject playerpub;
     private bool giveable = true;
+    public PuzzleObject parent;
     public PuzzleObject act;
     public int items;
+    public bool useX;
     private int current = 0;
     private int inHand = 0;
     private float move = -0.9f;
@@ -27,8 +29,17 @@ public class ItemHolder : PuzzleObject {
 
             for(int i = 0; i < loop; i++)
             {
-                GameObject spawned = GameObject.Instantiate(baseObject, GameObject.Find("KnifeHolder").transform, true);
-                spawned.transform.Translate(new Vector3(move, 0, 0));
+                GameObject spawned = GameObject.Instantiate(baseObject, parent.transform, true);
+                if (useX)
+                {
+                    spawned.transform.Translate(new Vector3(move, 0f, 0f));
+                }
+                else
+                {
+                    spawned.transform.Translate(new Vector3(0f, 0f, move));
+                }
+
+                spawned.transform.parent = parent.transform;
                 move = move - 0.8f;
                 current++;
                 inHand--;
