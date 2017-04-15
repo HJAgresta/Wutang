feae263070fdd0f1ce751bf2752daaa0a44c1dbd
Jake Recoon <jrecoon@gmail.com>
2017-04-15 18:30:30 -0400

    Safe room set up, not functional
    Just call activate() on the safe when you want it to open

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : PuzzleObject {
+
+    public int number;
+    private int counter;
+    private float timeOn = 0.7f;
+    private float timeOff = 0.3f;
+    private Light light;
+    private float changeTime = 0f;
+    private bool go;
+
+    // Use this for initialization
+    void Start()
+    {
+        go = false;
+        light = this.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Time.time > changeTime)
+        {
+            light.enabled = !light.enabled;
+            if (light.enabled)
+            {
+                changeTime = Time.time + timeOn;
+            }
+            else
+            {
+                changeTime = Time.time + timeOff;
+                counter++;
+            }
+        }
+
+        if(counter == number)
+        {
+            counter = 0;
+            changeTime += 2.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Safe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Safe : PuzzleObject {
+
+    public GameObject hinge;
+    private bool unlocked;
+
+	// Use this for initialization
+	void Start () {
+        unlocked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (unlocked)
+        {
+            if (this.transform.eulerAngles.y > 180)
+            {
+                this.transform.RotateAround(hinge.transform.position, Vector3.up, -Time.deltaTime * 30);
+            }
+        }
+	}
+
+    public override void activate()
+    {
+        unlocked = true;
+    }
+}
