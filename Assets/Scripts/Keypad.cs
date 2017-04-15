e2580e8016cfb9ad0f894419ce688880bc9bc422
Jake Recoon <jrecoon@gmail.com>
2017-04-15 16:37:03 -0400

    Security Room done, keypad coded

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : PuzzleObject {
+
+    public string combo;
+    private string entered;
+    public PuzzleObject act;
+
+    public string Entered
+    {
+        get { return entered; }
+    }
+
+    //add number to currently typed combo
+    public void addNum(string s)
+    {
+        if(s == "enter")
+        {
+            enter();
+        }
+        else if(s == "clear")
+        {
+            clear();
+        }
+        else
+        {
+            entered += s;
+        }
+    }
+
+    //check combo
+    void enter()
+    {
+        if(combo == entered)
+        {
+            act.activate();
+        }
+        else
+        {
+            clear();
+        }
+    }
+
+    //clear combo
+    void clear()
+    {
+        entered = "";
+    }
+}
---
+++ Assets/Scripts/KeypadNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadNumber : PuzzleObject {
+
+    public Keypad kP;
+    public string button;
+
+    public override void activate()
+    {
+        kP.addNum(button);
+    }
+}
