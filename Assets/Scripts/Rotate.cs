68c4ee5a8fa772048197efe28b3bc52c8538215a
Jake Recoon <jrecoon@gmail.com>
2017-04-15 22:05:03 -0400

    Every room is functional, finishing touches/fixes needed

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : PuzzleObject {
+
+    public PuzzleObject act;
+    private bool go;
+
+	// Use this for initialization
+	void Start () {
+        go = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (go && this.transform.eulerAngles.x < 89)
+        {
+            Debug.Log(this.transform.eulerAngles.x);
+            this.transform.Rotate(Time.deltaTime * 30, 0f, 0f);
+        }
+        else if(go && this.transform.eulerAngles.x >= 89)
+        {
+            act.activate();
+        }
+	}
+
+    public override void activate()
+    {
+        go = true;
+    }
+}
